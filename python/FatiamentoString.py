2bd1a3afd34cfc4d75ecedc774029a513d87aa8a
Sarah Cardoso <112648295+sarahcardoso1@users.noreply.github.com>
2024-09-08 22:09:51 -0300

    Delete python/FatiamentoString.py

--- python/FatiamentoString.py
+++
@@ -1,7 +0,0 @@
-nome = "Carlos Sainz Vázquez de Castro Cenamor Rincón Rebollo Virto Moreno de Aranda Don Per Urrielagoiria Pérez del Pulgar"
-
-print(nome[:10])
-print(nome[:9])
-print(nome[10:16])
-print(nome[10:16:2])
-print(nome[::-1])
